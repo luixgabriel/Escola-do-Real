f1560ab335e22b7077bc653c2f23efcd3c35dda0
Joelisson Santana <Joelisson997@gmail.com>
2023-08-16 21:48:37 -0300

    feature: criação do header e footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,43 @@
+import Image from 'next/image'
+
 export default function Footer() {
-  return <p>Footer</p>
+  return (
+    <footer className=" bg-gradient-to-r from-green-500 to-blue-500 pb-7 pt-16">
+      <div className="flex justify-center gap-4">
+        <Image
+          width={29}
+          height={29}
+          src="/icons/instagram.svg"
+          alt="logo do Instagram"
+        />
+        <Image
+          width={29}
+          height={29}
+          src="/icons/linkedin.svg"
+          alt="logo do Linkedin"
+        />
+        <Image
+          width={29}
+          height={29}
+          src="/icons/facebook.svg"
+          alt="logo do Facebook"
+        />
+      </div>
+      <small className="flex justify-center text-base leading-10 text-white">
+        &copy; 2023 - Escola do Real - Todos os direitos reservados
+      </small>
+      <span className="flex justify-center text-xs text-white ">
+        <span className="mr-1 ">Desenvolvido por </span>
+        <a
+          className="underline"
+          rel="external"
+          target="_blank"
+          href="https://www.linkedin.com/company/juniors-developers-group/mycompany/"
+        >
+          Juniors Developers Group.
+        </a>
+        {/* <Image src="#" alt="Logo Juniors Developers Group." height={29} width={29} /> */}
+      </span>
+    </footer>
+  )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
+'use client'
+
+import Image from 'next/image'
+
 export default function Header() {
-  return <p>Header</p>
+  return (
+    <header className=" flex items-center justify-around bg-zinc-100">
+      <Image
+        className="m-8"
+        src="/images/logo.png"
+        width={188}
+        height={68}
+        alt="Logo da escola do real"
+      />
+      <div>
+        <button>Fazer doacao</button>
+        <button>Faça parte</button>
+      </div>
+    </header>
+  )
 }
