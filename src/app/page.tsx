c087b648773a98651ca92c1c42e27759f868c029
Joelisson Santana <Joelisson997@gmail.com>
2023-08-18 20:31:51 -0300

    feature: adicionado sesao "onde atuamos"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import SectionLocation from '@/components/SectionLocation'
 import Banner from '@/components/banner'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navbar />
       <Banner />
+      <SectionLocation />
     </>
   )
 }
---
+++ src/components/SectionLocation.tsx
@@ -0,0 +1,11 @@
+import SectionCard from './SectionCard'
+import SectionTitle from './SectionTitle'
+
+export default function SectionLocation() {
+  return (
+    <section className="py-16">
+      <SectionTitle icon="/icons/location.svg">Onde Atuamos</SectionTitle>
+      <SectionCard />
+    </section>
+  )
+}
