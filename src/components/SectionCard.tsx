2553888a8d1e3aff824d68d9c4e8f2a948f7924a
Lucas Palhano Fonseca <lucaspalhanof@gmail.com>
2023-08-18 21:21:29 -0300

    style: Ajusta do estilo dos cards da seção Onde Atuamos

--- src/components/SectionCard.tsx
+++ src/components/SectionCard.tsx
@@ -1,52 +1,21 @@
 'use client'
 import Image from 'next/image'
 
-const data = [
-  {
-    src: '/icons/globe.svg',
-    alt: 'globe icon',
-    title: 'Estrutura',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-  },
-  {
-    src: '/icons/hands.svg',
-    alt: 'hands icon',
-    title: 'Estrutura',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-  },
-  {
-    src: '/icons/location.svg',
-    alt: 'location icon',
-    title: 'Estrutura',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-  },
-  {
-    src: '/icons/talk.svg',
-    alt: 'talk icon',
-    title: 'Estrutura',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-  },
-]
-
-export default function SectionCard() {
+export default function SectionCard({
+  item,
+}: {
+  item: {
+    src: string
+    alt: string
+    title: string
+    text: string
+  }
+}) {
   return (
-    <section className="custom-mx-global my-16 grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-4 ">
-      {data.map((item) => (
-        <section
-          key={item.title}
-          className="my-4 flex h-72 w-56 flex-col content-center items-center justify-between rounded-lg border-b-8 border-green-500 bg-gray-50 p-5 font-medium"
-        >
-          <Image
-            height={68}
-            width={68}
-            alt={item.alt}
-            src={item.src}
-            className="my-4"
-          />
-          <h3 className="text-xl font-bold text-green-500">{item.title}</h3>
-          <p className="text-sm font-medium">{item.text}</p>
-        </section>
-      ))}
-    </section>
+    <div className="my-4 flex flex-col content-center items-center justify-between gap-4 rounded-lg border-b-8 border-green-500 bg-gray-50 p-5 font-medium">
+      <Image height={52} width={52} alt={item.alt} src={item.src} />
+      <h3 className="text-xl font-semibold text-green-500">{item.title}</h3>
+      <p className="text-xs text-gray-600">{item.text}</p>
+    </div>
   )
 }
--- src/components/SectionLocation.tsx
+++ src/components/SectionLocation.tsx
@@ -1,11 +1,42 @@
 import SectionCard from './SectionCard'
 import SectionTitle from './SectionTitle'
 
+const data = [
+  {
+    src: '/icons/globe.svg',
+    alt: 'globe icon',
+    title: 'Estrutura',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+  },
+  {
+    src: '/icons/hands.svg',
+    alt: 'hands icon',
+    title: 'Estrutura',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+  },
+  {
+    src: '/icons/location.svg',
+    alt: 'location icon',
+    title: 'Estrutura',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+  },
+  {
+    src: '/icons/talk.svg',
+    alt: 'talk icon',
+    title: 'Estrutura',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+  },
+]
+
 export default function SectionLocation() {
   return (
     <section className="py-16">
       <SectionTitle icon="/icons/location.svg">Onde Atuamos</SectionTitle>
-      <SectionCard />
+      <section className="custom-mx-global my-16 grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 lg:grid-cols-4 ">
+        {data.map((item) => (
+          <SectionCard key={item.src} item={item} />
+        ))}
+      </section>
     </section>
   )
 }
