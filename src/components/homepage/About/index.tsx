64033fadb48598b1f06481bd6232bd96f5a6db4f
Rafael Lopes <rafael.lopesdesigner@gmail.com>
2023-08-22 20:17:12 -0300

    Adicao de ID para scroll

--- src/components/homepage/About/index.tsx
+++ src/components/homepage/About/index.tsx
@@ -2,7 +2,7 @@ import Title from '../../general/Title'
 
 export default function About() {
   return (
-    <section className='custom-py-section custom-mx-global'>
+    <section className='custom-py-section custom-mx-global' id="escola-do-real">
       <Title icon="/icons/globe.svg">Conheça a Escola do Real</Title>
       <p>A <strong>Escola do Real</strong> é uma instituição que busca educar os brasileros financeiramente e desmistificar diversos esteriótipos sobre dinheiro que permeiam nossa sociedade.</p>
       <p className='mt-3'>No Brasil, ensino religioso e química orgânica fazem parte do currículo escolar obrigatório, mas as finanças básicas, por exemplo, não. Segundo a Confederação Nacional do Comércio de Bens, Serviços e Turismo (CNC), cerca de 80% das famílias brasileiras sofrem com endividamento. Foi pensando nessas pessoas que criamos o projeto.</p>
