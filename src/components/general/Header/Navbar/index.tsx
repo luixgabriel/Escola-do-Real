46a9ad1f348e717c4a9ed3adcaf803ec60b6fbd7
Lucas Palhano Fonseca <lucaspalhanof@gmail.com>
2023-08-29 18:58:17 -0300

    feat: Implementa a navbar para rotas diferentes da root
    Ao acessar uma página que não a inicial, irá aparecer na navbar a opção de voltar para a página inicial. Além disso, na navbar ficará sempre disponível, não sendo necessário o sistema de toggle do burguer icon.

--- src/components/general/Header/Navbar/index.tsx
+++ src/components/general/Header/Navbar/index.tsx
@@ -1,3 +1,5 @@
+// TODO: Fazer os ajustes no campo de voltar para a página inicial
+
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -28,37 +30,60 @@ const data = [
   },
 ]
 
-export default function Navbar() {
+export default function Navbar({ isRoot }: { isRoot: boolean }) {
   return (
     <nav className="custom-mx-global flex flex-col pb-6">
-      <ul className="flex flex-col items-center justify-center gap-6 border-b-2 border-green-500 py-6 text-base font-bold md:flex-row md:justify-between">
-        {data.map((item) => (
-          <li key={item.href}>
-            <Link
-              href={item.href}
-              target="_parent"
-              className="flex h-full items-center gap-2"
-            >
-              <Image
-                className="max-h-8"
-                src={item.icon}
-                height={35}
-                width={35}
-                alt={item.alt}
-              />
-              <span className="text-blue-500">{item.text}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="mt-5 flex justify-between md:hidden">
-        <button className="rounded-md bg-blue-500 px-5 py-3 uppercase text-white hover:bg-blue-700">
-          Fazer Doação
-        </button>
-        <button className="rounded-md bg-green-500 px-5 py-3 uppercase text-white hover:bg-green-600">
-          Faça Parte
-        </button>
-      </div>
+      {isRoot ? (
+        <>
+          <ul className="flex flex-col items-center justify-center gap-6 border-b-2 border-green-500 py-6 text-base font-bold md:flex-row md:justify-between">
+            {data.map((item) => (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  target="_parent"
+                  className="flex h-full items-center gap-2"
+                >
+                  <Image
+                    className="max-h-8"
+                    src={item.icon}
+                    height={35}
+                    width={35}
+                    alt={item.alt}
+                  />
+                  <span className="text-blue-500">{item.text}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-5 flex justify-between md:hidden">
+            <button className="rounded-md bg-blue-500 px-5 py-3 uppercase text-white hover:bg-blue-700">
+              Fazer Doação
+            </button>
+            <button className="rounded-md bg-green-500 px-5 py-3 uppercase text-white hover:bg-green-600">
+              Faça Parte
+            </button>
+          </div>
+        </>
+      ) : (
+        <Link
+          href={'/'}
+          className="mt-6 flex gap-2 text-base font-bold text-blue-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-6 w-6 font-bold text-green-500"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>Página Inicial</span>
+        </Link>
+      )}
     </nav>
   )
 }
--- src/components/general/Header/index.tsx
+++ src/components/general/Header/index.tsx
@@ -2,13 +2,22 @@
 
 import Navbar from '@/components/general/Header/Navbar'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { useLayoutEffect, useState } from 'react'
 
 export default function Header() {
+  const pathname = usePathname()
+  const [isRoot, setIsRoot] = useState<boolean>(pathname === '/')
   const [open, setOpen] = useState<boolean>(false)
-  useEffect(() => {
-    if (window.innerWidth > 768) setOpen(true)
-  }, [])
+
+  useLayoutEffect(() => {
+    const mobileAndRoot = window.innerWidth < 768 && pathname === '/'
+    const desktopOrNotRoot = window.innerWidth >= 768 || pathname !== '/'
+    if (mobileAndRoot) setOpen(false)
+    if (desktopOrNotRoot) setOpen(true)
+    pathname === '/' ? setIsRoot(true) : setIsRoot(false)
+  }, [pathname])
+
   const handleClick = (): void => setOpen((prev) => !prev)
 
   return (
@@ -24,39 +33,41 @@ export default function Header() {
             priority
           />
         </a>
-        <div className="block md:hidden" onClick={handleClick}>
-          {open ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-10 w-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-10 w-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          )}
-        </div>
+        {isRoot && (
+          <div className="block md:hidden" onClick={handleClick}>
+            {open ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-10 w-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-10 w-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </div>
+        )}
         <div className="hidden md:block">
           <div className="flex gap-4">
             <button className="rounded-md bg-blue-500 px-5 py-3 text-sm uppercase text-white hover:bg-blue-700">
@@ -68,7 +79,7 @@ export default function Header() {
           </div>
         </div>
       </header>
-      {open && <Navbar />}
+      {open && <Navbar isRoot={isRoot} />}
     </>
   )
 }
