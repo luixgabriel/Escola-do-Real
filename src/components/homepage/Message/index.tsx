2dc4bb2dd33366726d4117b72802d28bfded8158
Luis Gabriel <luisgasbriel@gmail.com>
2024-04-18 20:46:19 -0300

    feat: finalizando projeto

--- src/components/homepage/Message/index.tsx
+++ src/components/homepage/Message/index.tsx
@@ -48,14 +48,17 @@ export default function Message() {
     e.preventDefault()
     setSending(true)
     try {
-      const result = await fetch(`https://escola-do-real.vercel.app`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+      const result = await fetch(
+        `https://escola-do-real.vercel.app/api/mensagem`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...state }),
         },
-        body: JSON.stringify({ ...state }),
-      })
+      )
       console.log(result)
       if (result.status === 201) {
         setState(cleanState)
